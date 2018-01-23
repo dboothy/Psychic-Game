92dc92229a9331a138992d456a364e607090d627
David Booth-Metwally <davidbmetwally@gmail.com>
2018-01-22 23:17:30 -0800

    final submission

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,83 +2,72 @@ var wins = 0;
 var losses = 0;
 var guessesLeft = 9;
 var guessesSoFar = [];
+var rand;
+
+
 
 var letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 
-var rand = letters[Math.floor(Math.random() * letters.length)];
-
-
-
-function gameStart(){
-
-var gameHtml = "<p>Wins:" + wins + "</p>"
-document.querySelector("#winsPlace").innerHTML = gameHtml;
-
-var gameHtml = "<p> Losses:" + losses + "</p>"
-document.querySelector("#lossesPlace").innerHTML = gameHtml;
-
-var gameHtml = "<p> Guesses left:" + guessesLeft + "</p>"
-document.querySelector("#guessesLeftPlace").innerHTML = gameHtml;
-
-var gameHtml = "<p> Guesses so far:" + guessesSoFar.join(','); + "</p>"
-document.querySelector("#guessesSoFarPlace").innerHTML = gameHtml;
-
-}
-
-
-function gameRestart(){
-
-var guessesLeft = 9;
-var guessesSoFar = [];
-
-}
 
 gameStart();
 
+
 document.onkeyup = function(event) {
-  var userChoice = event.key;
-      guessesLeft--;
+	var userChoice = event.key;
+	console.log(userChoice)
 
-guessesSoFar.push(userChoice);
+	if(letters.indexOf(userChoice) === -1){
+		return;
+	}
 
-  if(userChoice == rand){
-    checkAnswer(userChoice)
-  }
-}  
+	if(guessesSoFar.indexOf(userChoice) === -1) {
+		guessesSoFar.push(userChoice);
 
-function checkAnswer(userChoice){
-  if(userChoice == rand){
-    wins++; gameRestart();
 
-  }else(userChoice!== rand)
-    reduceGuessesLeft(userChoice)
-    
-  }
+	}
 
-function reduceGuessesLeft(userChoice){
-  if(userChoice !== rand){
-    guessesLeft--;
-  }if (guessesLeft == 0){
-    losses++; gameRestart();
-  }
+	guessesLeft --;
+	gameHtml =  "<p> Guesses so far: " + guessesSoFar.join(","); + "</p>"
+	document.querySelector("#guessesSoFarPlace").innerHTML= gameHtml;
+	gameHtml = "<p> Guesses left: " + guessesLeft + "</p>"
+	document.querySelector("#guessesLeftPlace").innerHTML= gameHtml;
 
+
+
+
+	if(userChoice.toLowerCase() === rand.toLowerCase()) {
+		wins++;
+		guessesLeft = 9;
+		guessesSoFar = [];
+		gameStart();
+
+	} else if(guessesLeft <=0) {
+		losses++;
+		guessesLeft = 9;
+		guessesSoFar = [];
+		gameStart();
+	}
 }
 
+function gameStart(){
+
+	 rand = letters[Math.floor(Math.random() * letters.length)]
+
+var gameHtml =	"<p>Wins: " + wins + "</p>"
+		document.querySelector("#winsPlace").innerHTML =gameHtml
+
+	gameHtml = "<p> Losses: " + losses + "</p>"
+	document.querySelector("#lossesPlace").innerHTML = gameHtml
+
+	gameHtml = "<p> Guesses left: " + guessesLeft + "</p>"
+	document.querySelector("#guessesLeftPlace").innerHTML= gameHtml;
+
+	gameHtml =  "<p> Guesses so far: " + guessesSoFar.join(","); + "</p>"
+	document.querySelector("#guessesSoFarPlace").innerHTML= gameHtml;
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
+};
 
 
 
